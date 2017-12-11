e32893773f7661c88a648c5ee36eb136edd02908
Dominik Kupaj <218147@edu.p.lodz.pl>
2017-12-11 01:13:27 +0100

    Add files via upload

---
+++ WyznaczanieSrMinMax.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void) {
+
+	int tablica[9];
+	int pobrana_liczba;
+	int i = 0;
+	int max = 0;
+	int srednia = 0;
+	int min = 0;
+
+	printf("Wprowadz 10 liczb\n");
+
+	for(i=0 ; i<10 ; i++) {
+		scanf("%i", &pobrana_liczba);
+		tablica[i] = pobrana_liczba;
+		max = tablica[i];
+		min = tablica[i];
+	}
+
+	for(i=0 ; i<10 ; i++) {
+
+		srednia = srednia + tablica[i];
+	}
+
+	srednia = srednia/10;
+
+	for(i=0 ; i<10 ; i++) {
+
+		if(tablica[i] > max) max = tablica[i];
+	}
+
+	for(i=0 ; i<10 ; i++) {
+
+		if(tablica[i] < min) min = tablica[i];
+	}
+
+	printf("\nWartosc maksymalna podanych liczb to: %i", max);
+	printf("\nWartosc srednia podanych liczb to: %i", srednia);
+	printf("\nWartosc minimalna podanych liczb to: %i", min);
+}
+
+
